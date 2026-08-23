090790cea7c197db7fcfd49e25d8842d4b02653e
DanielDeus89 <danielsilvadeus@gmail.com>
2026-08-23 20:10:40 -0300

    first commit

--- js/lesson78.js
+++ js/lesson78.js
@@ -1,31 +1,30 @@
 const lessonTitle = "Lesson 78";
-const currentVideoId = "TBPCRAhiKHY";
+const currentVideoId = "jRvmS5yc8Ec";
 
 const timeRanges = [
 
-  { start: 0, end: 130 }, 
-  { start: 130, end: 184 }, 
-  { start: 184, end: 335 }, 
-  { start: 335, end: 396 }, 
-  { start: 396, end: 529 }, 
-  { start: 529, end: 576 }, 
-  { start: 576, end: 804 }, 
-  { start: 804, end: 895 }, 
-  { start: 895, end: 907 }, 
-  { start: 907, end: 927 }, 
-  { start: 927, end: 943 }, 
-  { start: 943, end: 960 }, 
-  { start: 960, end: 974 }, 
-  { start: 974, end: 988 }, 
-  { start: 988, end: 1001 }, 
-  { start: 1001, end: 99999 }, 
+  { start: 7, end: 133 }, 
+  { start: 133 + 5, end: 189 }, 
+  { start: 188 + 5, end: 340 }, 
+  { start: 340 + 5, end: 399 }, 
+  { start: 398 + 5, end: 534 }, 
+  { start: 534 + 5, end: 580 }, 
+  { start: 580 + 5, end: 808 }, 
+  { start: 808 + 5, end: 895 }, 
+  { start: 895 + 5, end: 913 }, 
+  { start: 996, end: 1015 }, 
+  { start: 1015, end: 1031 }, 
+  { start: 1031, end: 1048 }, 
+  { start: 1048, end: 1062 }, 
+  { start: 1061, end: 1076 }, 
+  { start: 1076, end: 1088 }, 
+  { start: 1088, end: 99999 }, 
 
 
 ];
 
 const lessonCards = [
-  
-   {
+  {
    title: "Practice",
     columns: [
       [ 
@@ -165,86 +164,12 @@ const lessonCards = [
   "title": "Listen and Answer",
   "type": "listening",
   "segments": [
-    { "text": " ", "start": 1001, "end": 1008 },  
-    { "text": " ", "start": 1008, "end": 1012 },  
-    { "text": " ", "start": 1012, "end": 1015 },  
-    { "text": " ", "start": 1015, "end": 1021 },  
-    { "text": " ", "start": 1021, "end": 1026 }  
-  ]
-},
-{
-  "title": "Reading & Comprehension",
-  "text": "As soon as Joe stood up to answer the teacher's question, he knew that moment would certainly be important to his mother's decision about giving him the digital camera or not. Mrs. Coller, his literature teacher, asked him easy questions from the text, on the other hand she didn't allow him much time to answer. Fortunately everything went fine and Joe got a good grade, now he can take pictures and have his album collection each time bigger.",
-  "columns": [
-    [
-      [
-        "As soon as Joe stood up to answer the teacher's question, he knew that moment would certainly be important to his mother's decision about giving him the digital camera or not. Mrs. Coller, his literature teacher, asked him easy questions from the text, on the other hand she didn't allow him much time to answer. Fortunately everything went fine and Joe got a good grade, now he can take pictures and have his album collection each time bigger.",
-        ""
-      ]
-    ]
-  ]
-},
-{
-  "title": "Reading & Comprehension",
-  "text": "I will invite some friends to celebrate Jimmy's birthday with a surprise party. I need to get my phone book to call some friends who live far away. After lunch, I will go for a walk in the neighborhood to meet my brother-in-law in front of the public building, where he works. He will fast let the family know about the party. In the evening, I'll put on my blue dress and wait for my dear husband to arrive.",
-  "columns": [
-    [
-      ["1. She'll meet her brother-in-law in front of", ""],
-      ["a) the public library.", ""],
-      ["b) the public building.", ""],
-      ["c) her house.", ""]
-    ]
-  ]
-},
-{
-  "title": "Reading & Comprehension",
-  "text": "I will invite some friends to celebrate Jimmy's birthday with a surprise party. I need to get my phone book to call some friends who live far away. After lunch, I will go for a walk in the neighborhood to meet my brother-in-law in front of the public building, where he works. He will fast let the family know about the party. In the evening, I'll put on my blue dress and wait for my dear husband to arrive.",
-  "columns": [
-    [
-      ["2. It will be a", ""],
-      ["a) public party.", ""],
-      ["b) surprise birthday party.", ""],
-      ["c) family reunion.", ""]
-    ]
-  ]
-},
-{
-  "title": "Reading & Comprehension",
-  "text": "I will invite some friends to celebrate Jimmy's birthday with a surprise party. I need to get my phone book to call some friends who live far away. After lunch, I will go for a walk in the neighborhood to meet my brother-in-law in front of the public building, where he works. He will fast let the family know about the party. In the evening, I'll put on my blue dress and wait for my dear husband to arrive.",
-  "columns": [
-    [
-      ["3. Who will be invited to the party?", ""],
-      ["a) Family and friends.", ""],
-      ["b) Co-workers and family.", ""],
-      ["c) Friends and co-workers.", ""]
-    ]
-  ]
-},
-{
-  "title": "Reading & Comprehension",
-  "text": "I will invite some friends to celebrate Jimmy's birthday with a surprise party. I need to get my phone book to call some friends who live far away. After lunch, I will go for a walk in the neighborhood to meet my brother-in-law in front of the public building, where he works. He will fast let the family know about the party. In the evening, I'll put on my blue dress and wait for my dear husband to arrive.",
-  "columns": [
-    [
-      ["4. She will invite people", ""],
-      ["a) by email and a phone call.", ""],
-      ["b) by email and personally.", ""],
-      ["c) with a phone call and personally.", ""]
-    ]
-  ]
-},
-{
-  "title": "Who is Who?",
-  "columns": [
-    [
-      ["Whose birthday is it this month?", ""],
-      ["Who takes a walk every morning?", ""],
-      ["Who knows how to dance?", ""],
-      ["Who went to Disney?", ""],
-      ["Who has a passport?", ""],
-      ["Who knows how to cook?", ""]
-    ]
+    { "text": " ", "start": 1092, "end": 1095 },  
+    { "text": " ", "start": 1095, "end": 1100 },  
+    { "text": " ", "start": 1100, "end": 1103 },  
+    { "text": " ", "start": 1103, "end": 1109 },  
+    { "text": " ", "start": 1109, "end": 1109 }  
   ]
 }
-
 ];
 
